8bc3b4116870ecd6cafcc0bb55101054069f2755
Hieronymus <hieronymus@git.com>
2025-12-24 19:35:52 +0800

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-}) 
+  plugins: [svelte(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Secret Post",
+      short_name: "Secret Post",
+      description: "A secure way to share messages.",
+      theme_color: "#7c5cff",
+      icons: [
+        {
+          src: "/pwa-icon.png",
+          type: "image/png"
+        }
+      ]
+    }
+  })]
+})
